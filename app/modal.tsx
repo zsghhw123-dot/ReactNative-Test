439d4c285f0b5c945917e2cbe8dc0e9b448a8f89
zsghhw <zsghhw@icloud.com>
2025-10-03 18:13:57 +0800

    fix: 修复模态框标题多余空格

--- app/modal.tsx
+++ app/modal.tsx
@@ -7,7 +7,7 @@ import { ThemedView } from '@/components/themed-view';
 export default function ModalScreen() {
   return (
     <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
+      <ThemedText type="title">This is a modal </ThemedText>
       <Link href="/" dismissTo style={styles.link}>
         <ThemedText type="link">Go to home screen</ThemedText>
       </Link>
